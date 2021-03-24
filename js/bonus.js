1d0317e8b00f38d744bf43786401288ac8105607
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-03-24 16:34:35 +0200

    Задачи

--- js/bonus.js
+++ js/bonus.js
@@ -19,21 +19,60 @@ const users = [
      !!!Важно понимать пол пользователя и определить в сообщении "ему" или "ей"
 */
 
+const filterOfAge = (users) => {
+  const newUser = [...users]
+    .filter(user => user.age >= 18)
+    .sort((a, b) => a.age - b.age);
+  
+  const junior = newUser[0];
+  
+  if (junior.sex === "male") {
+    return `${junior.name} самый молодой и ему ${junior.age} лет`
+  } else
+    return `${junior.name} самая молодая и ей ${junior.age} лет`
+}
+
+console.log(filterOfAge(users));
+
+
+
+
+
+
+// const filterOfAge = users.filter(user => user.age >= 18)
+
+// console.log(filterOfAge);
+
+
 
 
 /* 
   2. В массиве users отфильтровать всех по полу, сначала female потом male 
 */
-const filterOfSex = (users) =>
-    users.filter(user => user.sex === 'female');
+const filterOfSex = (users,sex) =>
+    users.filter(user => user.sex === sex);
 
-console.log(filterOfSex(users));
+console.log(filterOfSex(users,'female'));
+console.log(filterOfSex(users,'male'));
+
+
+// or
+
+const SortedOfSex = [...users].sort((a, b) => a.sex.localeCompare(b.sex));
+console.log(SortedOfSex);
+
 
 
 
 /* 
   3. В массиве users найти пользователя по имени Марина
 */
+
+const findName = (users,name) =>
+    users.find(user => user.name === name)
+
+console.log(findName(users,'Марина'));
+
 
 
 
@@ -42,3 +81,17 @@ console.log(filterOfSex(users));
   если женщина снять 5 лет
   !!! Важно проверять что возраст не отрицательное число :)
 */
+
+const changeAge = (users) => {
+   users.map(user => {
+
+    if (user.sex === 'male' && user.age > 0) {
+      user.age += 10;
+    } else { user.age -= 5 && user.age > 0 }
+    return user.age
+  })
+    
+  return users;
+}
+
+console.log(changeAge(users));
