d68d89a7c713015823db9da838cb01182494fae4
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-03-31 16:01:35 +0300

    Задачи

--- js/bonys_Anrej.js
+++ js/bonys_Anrej.js
@@ -191,15 +191,17 @@ class Validator {
     return re.test(domain);
   }
 
-//     isDomain(date) {
-//   }
-  
-  // isPhone(phone) {
+  isDate(date) {
+    const [day, month, year] = date.split('.');
 
-  // }
+    if (Number(day) < 1 || Number(day) > 31) return false;
+    if (Number(month) < 1 || Number(month) > 12) return false;
+    if (Number(year) < 1900) return false;
 
+    return true;
+    
+  }
 }
-
 
 const validator = new Validator();
 
@@ -209,9 +211,52 @@ const validator = new Validator();
 // console.log(validator.isDomain('phphtml.net'));
 // console.log(validator.isDomain('phphtml/net'));
 
-// console.log(validator.isDate('12.05.2020'));
+console.log(validator.isDate('12.05.2020'));
+console.log(validator.isDate('32.05.2020'));
 
 // console.log(validator.isPhone('+375 (29) 817-68-92')); //тут можете формат своей страны
+
+
+
+// $teaser
+// var string = 'credit card number: 1234-2521-2315 credit card number2: 1234-2521-5897 ',
+//   regV = /\b[0-9]{4}-[0-9]{4}-[0-9]{4}\b/g;
+
+// console.log(string.match(regV));
+
+// let exp = new RegExp('title');
+// let exp = /sit/ig;
+// let str = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, obcaecati? Molestiae est neque magni? Laborum ullam facere consequatur perferendis quasi. Lorem ipsum dolor sit, amet consectetur adipisicing elit Sit. Eveniet, obcaecati? Molestiae est neque magni? Laborum ullam facere consequatur perferendis quasi.';
+// console.log(str.match(exp));
+// console.log(exp.test(str));
+
+// let exp = /\d/g;
+// let str = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit 2500 USD.';
+// console.log(exp.test(str));
+// console.log(str.match(exp));
+
+// let exp = /\W/g;
+// let str = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit 2500 USD.';
+// console.log(exp.test(str));
+// console.log(str.match(exp));
+
+// let exp = /2500/g;
+// let str = 'Lorem ipsum dolor a sit, amet consectetur adipisicing elit 2500 USD.';
+// console.log(exp.test(str));
+// console.log(str.match(exp));
+
+// let exp = /\b\d{4,}\b/g;
+// let str = '789 52 65 4558 123 657 5 788 964441 1';
+// console.log(str.match(exp));
+
+// let exp = /\b[a-z]{3}\b/g;
+// // let str = 'фцвфв фыв жфывждфл ыждвлждылждвлфы ждлафжалфывлфыждвлфыжд влфыжлвж';
+// let str = 'Lorem ipsum dolor a sit, amet consectetur adipisicing elit 2500 USD.';
+// console.log(str.match(exp));
+
+// let exp = /^[a-z]+\.js$/g;
+// let str = 'dgds index.js';
+// console.log(str.match(exp));
 
 
 
